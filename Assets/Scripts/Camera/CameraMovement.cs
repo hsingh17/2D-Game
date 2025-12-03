0e82e4639dbb21eb6ccec452bfc1926969a4ad1d
Harjot Singh <hsingh172000@gmail.com>
2025-12-03 12:34:22 -0800

    Offset camera slightly

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,19 +1,45 @@
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Vector3 offset;
+
     [SerializeField]
     private Transform target;
 
     [SerializeField]
     private float smoothTime;
 
+    [SerializeField]
+    private Tilemap backgroundTilemap;
+
     private Vector3 velocity = Vector3.zero;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = gameObject.GetComponent<Camera>();
+    }
 
     private void LateUpdate()
     {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        Debug.Log($"{camera.orthographicSize}");
+        // Don't move camera if not inside the bounds of the background
+        if (!TileMapContainsCamera())
+        {
+            return;
+        }
+
         Vector3 currentPos = transform.position;
-        Vector3 targetPos = target.transform.position;
+        Vector3 targetPos = target.transform.position + offset;
 
         currentPos.z = targetPos.z;
 
@@ -23,5 +49,20 @@ public class CameraMovement : MonoBehaviour
             ref velocity,
             smoothTime
         );
+    }
+
+    private bool TileMapContainsCamera()
+    {
+        return true;
+        // Bounds tilemapBounds = backgroundTilemap.localBounds;
+        // Vector3 currentPos = transform.position;
+        // Vector3 top = transform.position;
+        // Vector3 bottom;
+        // Vector3 left;
+        // Vector3 right;
+        // return tilemapBounds.Contains()
+        //     && tilemapBounds.Contains()
+        //     && tilemapBounds.Contains()
+        //     && tilemapBounds.Contains();
     }
 }
